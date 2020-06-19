31b5e318d7af10ad348807a4226b694a4c2b3972
Mauro Ficorella <mauro.ficorella94@gmail.com>
2020-06-19 10:36:53 +0200

    updated css

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 //file js
 
+var fov = 75;
+var aspect = 2;
+var near = 0.1;
+var far = 5;
+
+var cameraX = 0;
+var cameraY = 0;
+var cameraZ = 2;
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(cameraX, cameraY, cameraZ);
+
+    
+}
