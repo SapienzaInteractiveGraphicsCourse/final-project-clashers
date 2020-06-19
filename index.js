f9941966fd9491410c6dc3e54975713147c5c220
Mauro Ficorella <mauro.ficorella94@gmail.com>
2020-06-19 11:51:54 +0200

    updated cube

--- index.js
+++ index.js
@@ -1,51 +1,67 @@
 //file js
 
-import * as THREE from './build/three.module.js';
-
-var fov = 75;
-var aspect = 2;
-var near = 0.1;
-var far = 5;
-
-var cameraX = 0;
-var cameraY = 0;
-var cameraZ = 2;
-
-const color = 0xFFFFFF;
-const intensity = 1;
-
-//cubo di prova
-const boxWidth = 1;
-const boxHeight = 1;
-const boxDepth = 1;
+//import * as THREE from './build/three.module.js';
 
 function init() {
-    scene = new THREE.Scene();
+    const canvas = document.querySelector('#c');
+    renderer = new THREE.WebGLRenderer({
+        canvas
+    }); //controllare se serve l'antialias
+
+    const fov = 75;
+    const aspect = 2;
+    const near = 0.1;
+    const far = 5;
+
+    const cameraX = 0;
+    const cameraY = 0;
+    const cameraZ = 2;
+
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(cameraX, cameraY, cameraZ);
 
     //var container = document.getElementById('game'); -> controllare a che serve
 
-    renderer = new THREE.WebGLRenderer({
-        canvas
-    }); //controllare se serve l'antialias
+    const scene = new THREE.Scene();
 
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const dirLight = new THREE.DirectionalLight(color, intensity);
+        dirLight.position.set(-1, 2, 4);
+        scene.add(dirLight);
+    }
+
+    //cubo di prova
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
     const material = new THREE.MeshBasicMaterial({
         color: 0x44aa88
     });
 
-    const cube = new THREE.Mesh(geometry,material);
+    const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    var dirLight = new THREE.DirectionalLight(color, intensity);
-    dirLight.position.set(-1, 2, 4);
-    scene.add(dirLight);
+    
 
-    var ambientLight = new THREE.AmbientLight(color, intensity);
-    scene.add(ambientLight);
 
-    renderer.render(scene, camera);
+    //var ambientLight = new THREE.AmbientLight(color, intensity);
+    //scene.add(ambientLight);
+    function render(time) {
+        time *= 0.001; // convert time to seconds
+
+        cube.rotation.x = time;
+        cube.rotation.y = time;
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+
+
 }
 
 init();
