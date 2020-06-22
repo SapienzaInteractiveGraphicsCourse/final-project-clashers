1f25dfb485f29ce001a0323d64309b979d487cff
Mauro Ficorella <mauro.ficorella94@gmail.com>
2020-06-22 17:01:57 +0200

    various modifications

--- index.js
+++ index.js
@@ -1,141 +1,74 @@
 //file js
 
-//import * as THREE from './build/three.module.js';
-
-/*function loadModel() {
-    // Instantiate a loader
-    var loader = new THREE.GLTFLoader();
-
-    // Load a glTF resource
-    loader.load(
-        // resource URL
-        "./models/crash.gltf",
-        // called when the resource is loaded
-        function(gltf) {
-            scene.add(gltf.scene);
-
-            gltf.animations; // Array<THREE.AnimationClip>
-            gltf.scene; // THREE.Group
-            gltf.scenes; // Array<THREE.Group>
-            gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-        },
-        // called while loading is progressing
-        function(xhr) {
-            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-        },
-        // called when loading has errors
-        function(error) {
-            console.log("An error happened");
-        }
-    );
-}*/
+import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js";
+import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r115/examples/jsm/loaders/GLTFLoader.js";
 
 function init() {
-    //const canvas = document.querySelector('#c');
+  //const canvas = document.querySelector('#c');
+  var container = document.getElementById("game"); //-> controllare a che serve
+  var renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  }); //controllare se serve l'antialias
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var container = document.getElementById("game"); //-> controllare a che serve
-    renderer = new THREE.WebGLRenderer({
-        antialias: true,
-    }); //controllare se serve l'antialias
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  const cameraX = 0;
+  const cameraY = 0;
+  const cameraZ = 15;
 
-    const fov = 75;
-    const aspect = 2;
-    const near = 0.1;
-    const far = 5;
+  //camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(cameraX, cameraY, cameraZ);
 
-    const cameraX = 0;
-    const cameraY = 0;
-    const cameraZ = 2;
+  window.addEventListener(
+    "resize",
+    function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    },
+    false
+  );
 
-    //camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-    );
-    camera.position.set(cameraX, cameraY, cameraZ);
+  container.appendChild(renderer.domElement);
 
-    window.addEventListener(
-        "resize",
-        function() {
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
-            renderer.setSize(window.innerWidth, window.innerHeight);
-        },
-        false
-    );
+  const scene = new THREE.Scene();
 
-    container.appendChild(renderer.domElement);
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const dirLight = new THREE.DirectionalLight(color, intensity);
+    dirLight.position.set(-1, 2, 4);
+    scene.add(dirLight);
+  }
 
-    const scene = new THREE.Scene();
+  var root = new THREE.Scene();
 
-    {
-        const color = 0xffffff;
-        const intensity = 1;
-        const dirLight = new THREE.DirectionalLight(color, intensity);
-        dirLight.position.set(-1, 2, 4);
-        scene.add(dirLight);
-    }
-
-    //cubo di prova
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    const material = new THREE.MeshPhongMaterial({
-        color: 0x44aa88,
+  {
+    const gltfLoader = new GLTFLoader();
+    const url = "models/crash/scene.gltf";
+    gltfLoader.load(url, (gltf) => {
+      root = gltf.scene;
+      scene.add(root);
     });
+  }
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+  //var ambientLight = new THREE.AmbientLight(color, intensity);
+  //scene.add(ambientLight);
+  function render(time) {
+    time *= 0.001; // convert time to seconds
 
-    // Instantiate a loader
-    var loader = new THREE.GLTFLoader();
-    loader.crossOrigin = true;
+    renderer.render(scene, camera);
 
-    // Load a glTF resource
-    loader.load(
-        // resource URL
-        'models/crash.gltf',
-        // called when the resource is loaded
-        function(gltf) {
-            scene.add(gltf.scene);
-
-            gltf.animations; // Array<THREE.AnimationClip>
-            gltf.scene; // THREE.Group
-            gltf.scenes; // Array<THREE.Group>
-            gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-        },
-        // called while loading is progressing
-        function(xhr) {
-            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-        },
-        // called when loading has errors
-        function(error) {
-            console.log("An error happened");
-        }
-    );
-
-
-    //var ambientLight = new THREE.AmbientLight(color, intensity);
-    //scene.add(ambientLight);
-    function render(time) {
-        time *= 0.001; // convert time to seconds
-
-        cube.rotation.x = time;
-        cube.rotation.y = time;
-
-        renderer.render(scene, camera);
-
-        requestAnimationFrame(render);
-    }
     requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 
-    //loadModel();
+  //loadModel();
 }
 
 init();
