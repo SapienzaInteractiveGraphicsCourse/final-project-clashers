c8ae230f62e64ce849f0b9859a5391fbdcc92f2f
Mauro <mauro.ficorella94@gmail.com>
2020-06-24 10:27:20 +0200

    modified audio

--- audio.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js";
-
-var audioLoader = new THREE.AudioLoader();
-audioLoader.load("audio/Super Mario Bros. Soundtrack.mp3", function (buffer) {
-  sound.setBuffer(buffer);
-  sound.setLoop(true);
-  sound.setVolume(0.5);
-  sound.play();
-});
