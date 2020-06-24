eea404fedb3a6b21762e48f0063659814b49507a
Mauro <mauro.ficorella94@gmail.com>
2020-06-24 17:12:38 +0200

    pointlight

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/th
 import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources/threejs/r115/examples/jsm/controls/OrbitControls.js";
 
 var head;
+var scene;
 
 function init() {
   //const canvas = document.querySelector('#c');
@@ -29,6 +30,19 @@ function init() {
     1000
   );
 
+  /*var camera = new THREE.OrthographicCamera(
+    /*width / -2,
+    width / 2,
+    height / 2,
+    height / -2,
+    -50,
+    50,
+    50,
+    -50,
+    1,
+    1000
+  );*/
+
   var controls = new OrbitControls(camera, renderer.domElement);
 
   //controls.update() must be called after any manual changes to the camera's transform
@@ -47,32 +61,47 @@ function init() {
 
   container.appendChild(renderer.domElement);
 
-  var scene = new THREE.Scene();
+  scene = new THREE.Scene();
   scene.background = new THREE.Color(0xffffff);
 
   {
     const color = 0xffffff;
-    const intensity = 1;
+    const intensity = 0.5;
     var dirLight = new THREE.DirectionalLight(color, intensity);
-    dirLight.position.set(-1, 2, 10);
-    dirLight.castShadow = true;
+    dirLight.position.set(0, 5, 0);
+    /*dirLight.castShadow = true;
+
+    dirLight.shadow.mapSize.width = window.devicePixelRatio; // default
+    dirLight.shadow.mapSize.height = window.devicePixelRatio; // default
+
+    dirLight.shadow.camera = new THREE.OrthographicCamera(
+      -10,
+      10,
+      10,
+      -10,
+      0.5,
+      1000
+    );
+    dirLight.shadow.camera.near = 0.5; // default
+    dirLight.shadow.camera.far = 8000; // default
+    dirLight.shadow.camera.fov = 30;*/
+
     scene.add(dirLight);
 
-    dirLight.shadow.mapSize.width = 512; // default
-    dirLight.shadow.mapSize.height = 512; // default
-    dirLight.shadow.camera.near = 0.5; // default
-    dirLight.shadow.camera.far = 500; // default
-
     var ambientLight = new THREE.AmbientLight(color, intensity);
-    //ambientLight.castShadow = true;
     scene.add(ambientLight);
 
-    //Set up shadow properties for the light
-    /*ambientLight.shadow.mapSize.width = 512; // default
-    ambientLight.shadow.mapSize.height = 512; // default
-    ambientLight.shadow.camera.near = 0.5; // default
-    ambientLight.shadow.camera.far = 500; // default
-    */
+    var shadowLight = new THREE.PointLight(0xffffff, 1, 0, 2);
+    shadowLight.position.set(0, 100, 30);
+    shadowLight.castShadow = true;
+    shadowLight.shadowDarkness = 1;
+    shadowLight.shadow.camera.left = -100;
+    shadowLight.shadow.camera.right = 100;
+    shadowLight.shadow.camera.top = 100;
+    shadowLight.shadow.camera.bottom = -100;
+    shadowLight.shadow.camera.near = 0.1;
+    shadowLight.shadow.camera.far = 1000;
+    scene.add(shadowLight);
   }
 
   var yoshi = new THREE.Scene();
@@ -88,8 +117,15 @@ function init() {
       yoshi.name = "yoshi";
       yoshi.position.set(0, 0, -0.75);
       yoshi.scale.set(0.3, 0.3, 0.3);
-      yoshi.castShadow = true;
-      yoshi.receiveShadow = false;
+      yoshi.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
+
+      //yoshi.castShadow = true;
+      //yoshi.receiveShadow = false;
 
       head = yoshi.getObjectByName(yoshi_dic.Testa);
 
@@ -97,20 +133,12 @@ function init() {
 
       scene.add(yoshi);
       requestAnimationFrame(render);
-      console.log(head);
-    });
-
-    gltfLoader.load(url_world, (gltf) => {
-      world = gltf.scene;
-      world.name = "world";
-      world.position.set(0, 0, -0.75);
-      world.castShadow = true;
-      world.receiveShadow = true;
-      scene.add(world);
-      //requestAnimationFrame(render);
       //console.log(head);
     });
   }
+
+  scene.add(camera);
+  CreateLandscape();
 
   function render(time) {
     time *= 0.001;
@@ -119,7 +147,26 @@ function init() {
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
+
   //requestAnimationFrame(render);
+}
+
+var Landscape = function () {
+  var geometry = new THREE.BoxGeometry(window.innerWidth, 1, window.innerWidth);
+  var texture = new THREE.TextureLoader().load("img/desert.jpg");
+  var material = new THREE.MeshPhongMaterial({
+    map: texture,
+    color: 0xd2b48c,
+  });
+  this.mesh = new THREE.Mesh(geometry, material);
+  this.mesh.receiveShadow = true;
+};
+
+var landscape;
+function CreateLandscape() {
+  landscape = new Landscape();
+  landscape.mesh.position.y = -6;
+  scene.add(landscape.mesh);
 }
 
 init();
