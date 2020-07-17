1c39229ce25d8e2b44fe1822f9ec9553c0f46bd6
martinaturbessi <martina.turbessi95@gmail.com>
2020-07-17 11:59:55 +0200

    mario_dic

--- mario.js
+++ mario.js
@@ -1,11 +1,13 @@
 const mario_dic = {
   Head: "mixamorigHead_05",
-  Torso: "",
-  Spine: "",
+  Torso: "RootNode",
+  Spine: "mixamorigSpine1_02",
   UpperArm_right: "mixamorigRightArm_012",
   UpperArm_left: "mixamorigLeftArm_08",
   UpperLeg_right: "mixamorigRightUpLeg_020",
+  LowerLeg_right: "mixamorigRightLeg_021",
   UpperLeg_left: "mixamorigLeftUpLeg_015",
+  LowerLeg_left: "mixamorigLeftLeg_016",
   Hand_left: "mixamorigLeftHand_010",
   Hand_right: "mixamorigRightHand_014",
 };
